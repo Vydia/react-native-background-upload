0a78821498dea4310832fd8fa84cc78b3f17ffcb
Michael Radionov <michael.radionov@oxagile.com>
2017-02-15 11:39:51 +0300

    Return promise when cancelling upload on iOS

--- ios/VydiaRNFileUploader.m
+++ ios/VydiaRNFileUploader.m
@@ -150,7 +150,7 @@ RCT_EXPORT_METHOD(startUpload:(NSDictionary *)options resolve:(RCTPromiseResolve
  * Accepts upload ID as a first argument, this upload will be cancelled
  * Event "cancelled" will be fired when upload is cancelled.
  */
-RCT_EXPORT_METHOD(cancelUpload: (NSString *)cancelUploadId) {
+RCT_EXPORT_METHOD(cancelUpload: (NSString *)cancelUploadId resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     NSURLSession *session = [self urlSession];
     [session getTasksWithCompletionHandler:^(NSArray *dataTasks, NSArray *uploadTasks, NSArray *downloadTasks) {
         for (NSURLSessionTask *uploadTask in uploadTasks) {
@@ -159,6 +159,7 @@ RCT_EXPORT_METHOD(cancelUpload: (NSString *)cancelUploadId) {
             }
         }
     }];
+    resolve([NSNumber numberWithBool:YES]);
 }
 
 - (NSURLSession *)urlSession{
