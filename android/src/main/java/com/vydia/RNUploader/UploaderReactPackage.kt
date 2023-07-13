28cbb87729da683fd605413ea1f9befaa7b17f8a
Angel Rodriguez <angelmanuelrs@gmail.com>
2023-07-13 16:21:59 -0400

    refactor react package file to kotlin

---
+++ android/src/main/java/com/vydia/RNUploader/UploaderReactPackage.kt
@@ -0,0 +1,18 @@
+package com.vydia.RNUploader;
+
+import android.view.View
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+
+class UploaderReactPackage : ReactPackage {
+    override fun createViewManagers(
+        reactContext: ReactApplicationContext
+    ): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
+
+    override fun createNativeModules(
+        reactContext: ReactApplicationContext
+    ): MutableList<NativeModule> = listOf(UploaderModule(reactContext)).toMutableList()
+}
